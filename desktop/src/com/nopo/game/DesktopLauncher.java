2a9d1ba9a84191b9221783496b1ffcaa7da29c4e
NopoTheGamer <noahogno@gmail.com>
2022-04-13 17:39:35 +1000

    rain drop working

---
+++ desktop/src/com/nopo/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.nopo.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("nopo is cool");
+		config.setWindowedMode(800, 480);
+		config.useVsync(true);
+		config.setForegroundFPS(60);
+		new Lwjgl3Application(new Game(), config);
+	}
+}
