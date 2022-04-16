1c4dd68e5fb3212fc60c3df2a6be535a48f3f06a
NopoTheGamer <noahogno@gmail.com>
2022-04-16 14:50:56 +1000

    new font

--- core/src/com/nopo/game/Game.java
+++ core/src/com/nopo/game/Game.java
@@ -1,7 +1,10 @@
 package com.nopo.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
 public class Game extends com.badlogic.gdx.Game {
 
@@ -10,7 +13,18 @@ public class Game extends com.badlogic.gdx.Game {
 
     public void create() {
         batch = new SpriteBatch();
-        font = new BitmapFont(); // use libGDX's default Arial font
+        // Uses a ttf font so i can scale it up without it looking like doody water
+        // well i mean it still does
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("OpenSans-Regular.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 30;
+        parameter.borderWidth = 1;
+        parameter.color = Color.WHITE;
+        parameter.shadowOffsetX = 3;
+        parameter.shadowOffsetY = 3;
+        parameter.shadowColor = new Color(0, 0, 0, 1);
+        font = generator.generateFont(parameter);
+        generator.dispose();
         this.setScreen(new MainMenuScreen(this));
     }
 
